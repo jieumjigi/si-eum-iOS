1eb0805b2e501e05c3c474f473eba0a4b8b49b60
SeongHo <hsh@dotincorp.com>
2017-02-06 20:53:48 +0900

    create project

---
+++ sieum/sieum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sieum
+//
+//  Created by 홍성호 on 2017. 2. 6..
+//  Copyright © 2017년 홍성호. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
