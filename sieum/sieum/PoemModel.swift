df8a7118cd711adb586e55aca99d6e048d801683
SeongHo <hsh@dotincorp.com>
2017-05-17 23:47:32 +0900

    메뉴 버튼 기존 색상으로 변경

---
+++ sieum/sieum/PoemModel.swift
@@ -0,0 +1,25 @@
+//
+//  PoemModel.swift
+//  sieum
+//
+//  Created by 홍성호 on 2017. 5. 17..
+//  Copyright © 2017년 홍성호. All rights reserved.
+//
+
+import UIKit
+
+class PoemModel: NSObject {
+    
+    var poemId : String?
+    var pushDueDate : String?
+    
+    var title : String?
+    var poetName : String?
+    var contents : String?
+    var question : String?
+    
+    var introPoet : String?
+    var linkToBook : String?
+    
+
+}
