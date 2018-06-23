825c10cb409a6d8d797c5458bbddfb2117cd82d0
홍성호 <hsh3592@gmail.com>
2018-06-23 23:37:48 +0900

    main 화면 기반 작업 - 좌측에 side menu button - 앱 실행 시 최초 노출됨

---
+++ Pods/Target Support Files/Pods-sieum-SieumWidget/Pods-sieum-SieumWidget-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sieum_SieumWidget : NSObject
+@end
+@implementation PodsDummy_Pods_sieum_SieumWidget
+@end
---
+++ sieum/ColorUtil.swift
@@ -0,0 +1,62 @@
+//
+//  ColorUtil.swift
+//  sieum
+//
+//  Created by 홍성호 on 2017. 2. 12..
+//  Copyright © 2017년 홍성호. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor{
+    
+    class func defaultBackground() -> UIColor{
+        
+        return UIColor(red: (253/255.0), green: (255/255.0), blue: (251/255.0), alpha: 1.0)
+    }
+    
+    class func alertBackground() -> UIColor{
+        
+        return UIColor(red: (78/255.0), green: (78/255.0), blue: (78/255.0), alpha: 1.0)
+    }
+    
+    class func lightBrown() -> UIColor{
+        
+        return UIColor(red: (203/255.0), green: (200/255.0), blue: (193/255.0), alpha: 1.0)
+    }
+    
+    class func darkBrown() -> UIColor{
+        
+        return UIColor(red: (78/255.0), green: (78/255.0), blue: (78/255.0), alpha: 1.0)
+    }
+}
+
+class ColorUtil: UIColor {
+    
+    /**
+     
+     밝은색인지 체크해주는 함수
+     - parameter targetColor: 측정하고싶은 색상
+     - returns : 밝으면 true, 어두우면 false
+     - seealso: http://stackoverflow.com/questions/2509443/check-if-uicolor-is-dark-or-bright
+     */
+    func isLight(targetColor : CGColor) -> Bool
+    {
+        let components = targetColor.components
+        let componentRed = (components?[0])! * 299
+        let componentBlue = (components?[1])! * 587
+        let componentGreen = (components?[2])! * 114
+        
+        let brightness = (componentRed + componentBlue + componentGreen) / 1000
+        
+        if brightness < 0.5
+        {
+            return false
+        }
+        else
+        {
+            return true
+        }
+    }
+
+}
